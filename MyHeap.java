67135318fe9c954b269f131de4b4e546ca683e05
Derek <dyin00@stuy.edu>
2019-04-29 11:32:35 -0400

    swp method

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,20 @@
 public class MyHeap{
-  
+  private static void swap(int[] data, int s, int f){
+    int start = data[s];
+    data[s] = data[f];
+    data[f] = start;
+  }
+  private static void pushDown(int[]data,int size,int index){
+
+  }
+  private static void pushUp(int[]data,int index){
+
+  }
+  public static void heapify(int[] data){
+
+  }
+  public static void heapsort(int[] data){
+
+  }
+
 }
