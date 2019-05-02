6d3fe1d505dac2677f9a480b61b7e29e32831a12
Derek <dyin00@stuy.edu>
2019-05-01 23:04:40 -0400

    push down testing

--- MyHeap.java
+++ MyHeap.java
@@ -5,10 +5,29 @@ public class MyHeap{
     data[f] = start;
   }
   private static void pushDown(int[]data,int size,int index){
-    if (size==0) return;
-    else if (data[index] < data[index+2]){
-      swap(data, index, index+2);
-      pushDown(data, size-1, index++);
+    while (true){
+      int left = index * 2 + 1;
+      int right = left + 1;
+      if (right < size){
+        if (data[left] > data[right] && data[left] > data[index]){
+          swap(data, index, left);
+          index = left;
+        }
+        else return;
+
+        else if (data[right] > data[index]){
+          swap(data, index, right);
+          index = right;
+        }
+        else return;
+      }
+      else if (right == size){
+        if (data[left]>data[index]){
+          swap(data, index, left);
+          index = left;
+        }
+      }
+      return;
     }
   }
   private static void pushUp(int[]data,int index){
